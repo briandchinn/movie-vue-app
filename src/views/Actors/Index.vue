786c3fcda841c24fc01d1e7d8080e59d1ce03e45
briandchinn <bchinn310@gmail.com>
2019-06-23 14:40:08 -0700

    adds sort filter and animations to actor index

--- src/views/Actors/Index.vue
+++ src/views/Actors/Index.vue
@@ -2,13 +2,21 @@
   <div class="actors-index">
     <h3> {{ message }}</h3>
 
-    <div v-for="actor in actors">
-      <h4>{{ actor.first_name}} {{ actor.last_name}}</h4>
-      <button>
-        <router-link v-bind:to="'/actors/' + actor.id">More Info</router-link>
-      </button>
-      <hr>
-    </div>
+    Search by name: <input v-model="nameFilter" list="names">
+
+    <datalist id="names">
+      <option v-for="actor in actors"> {{ actor.last_name }}</option>
+    </datalist>
+    
+    <transition-group appear enter-active-class="animated bounce" leave-active-class="animated fadeOut">
+      <div v-for="actor in orderBy(filterBy(actors, nameFilter, 'last_name'),'last_name')" v-bind:key="actor.id">
+        <h4>{{ actor.first_name}} {{ actor.last_name}}</h4>
+        <button>
+          <router-link v-bind:to="'/actors/' + actor.id">More Info</router-link>
+        </button>
+        <hr>
+      </div>
+    </transition-group>
 
   </div>
 </template>
@@ -20,12 +28,15 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       actors: [],
-      message: "All actors"
+      message: "All actors",
+      nameFilter:''
     };
   },
   created: function() {
